c05c700b3c64b4dcb9203f0331f0edb60104caae
AshwiniSherki <126765641+AshwiniSherki@users.noreply.github.com>
2024-03-16 14:35:26 +0530

    Update shop.js

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -7,7 +7,7 @@ export const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-        <h1>PedroTech Shop</h1>
+        <h1>ElectroMart</h1>
       </div>
 
       <div className="products">
