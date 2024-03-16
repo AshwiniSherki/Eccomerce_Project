2331ac1ae8cf8c434e6bd02cffa57d181f6cc7ff
AshwiniSherki <126765641+AshwiniSherki@users.noreply.github.com>
2024-03-16 15:21:23 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,8 +7,8 @@ import { Link }   from "react-router-dom";
         <div className="navbar">
       <div className="links">
 
-        <link to="/">Shop</link>
-        <link to="/cart">U+F242</link>
+        <Link to="/">Shop</Link>
+        <Link to="/cart">U+F242</Link>
 
       </div>
         </div>
